c5717975c17ea10c7acc8dd77a4de6739d423e09
lightmnd <booking.djonly@gmail.com>
2022-12-22 12:29:10 +0100

    feature product style fixing

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -20,46 +20,70 @@ export default function Card(props: FeaturedProps) {
   return (
     <>
       <div className="featuredContainer">
-        <div className="textContainer">
-          <h3>{toUppercase}</h3>
+        <div className="featuredContainerWrapper">
+          <div className="textContainer">
+            <h3>{toUppercase}</h3>
+          </div>
+          <div className="textContainer">
+            <span>{description}</span>
+          </div>
         </div>
-        <div className="textContainer">
-          <span>{description}</span>
-        </div>
-      </div>
 
-      <div className="featuredImagesContainer">
-        <div className="featuredImages">
-          <>
-            {fashionData.map((item, index) => {
-              let random = Math.floor(Math.random() * 20);
-              if (index < 4) {
-                // <img src="https://via.placeholder.com/200x300" alt="" />;
-                return (
-                  <>
-                    {item.rating.rate >= 4 ? (
-                      <>
-                        <span className="top-rated">Top rated!</span>
-                        <img
-                          key={`${item + index}`}
-                          src={fashionData[random].image}
-                          alt=""
-                        />
-                      </>
-                    ) : (
-                      <img
-                        key={`${item + index}`}
-                        src={fashionData[random].image}
-                        alt=""
-                      />
-                    )}
-                  </>
-                );
-              }
-            })}
-            {/* <img src="https://via.placeholder.com/200x300" alt="" />
+        <div className="featuredImagesContainer">
+          <div className="featuredImages">
+            <>
+              {fashionData.map((item, index) => {
+                let random = Math.floor(Math.random() * 20);
+                if (index < 4) {
+                  // <img src="https://via.placeholder.com/200x300" alt="" />;
+                  return (
+                    <>
+                      {item.rating.rate >= 4 ? (
+                        <>
+                          <span className="topRated">Top rated!</span>
+                          <div className="featuredImagesWrapper">
+                            <img
+                              key={`${item + index}`}
+                              src={fashionData[random].image}
+                              alt=""
+                            />
+                            <div className="itemInfoContainer">
+                              <span className="itemTitle">{item.title}</span>
+                              <div className="itemInfoContainerPrice">
+                                <span className="originalPrice">
+                                  {item.price}
+                                </span>
+                                <span className="finalPrice">{item.price}</span>
+                              </div>
+                            </div>
+                          </div>
+                        </>
+                      ) : (
+                        <div className="featuredImagesWrapper">
+                          <img
+                            key={`${item + index}`}
+                            src={fashionData[random].image}
+                            alt=""
+                          />
+                          <div className="itemInfoContainer">
+                            <span className="itemTitle">{item.title}</span>
+                            <div className="itemInfoContainerPrice">
+                              <span className="originalPrice">
+                                {item.price}
+                              </span>
+                              <span className="finalPrice">{item.price}</span>
+                            </div>
+                          </div>
+                        </div>
+                      )}
+                    </>
+                  );
+                }
+              })}
+              {/* <img src="https://via.placeholder.com/200x300" alt="" />
             <img src="https://via.placeholder.com/200x300" alt="" /> */}
-          </>
+            </>
+          </div>
         </div>
       </div>
     </>
